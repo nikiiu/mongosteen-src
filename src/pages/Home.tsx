90216c7674fff176602033801763902deea790f1
nikiiu <2543954706@qq.com>
2024-07-10 22:41:16 +0800

    feat: 🎸 完成首页样式

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,19 @@
+import pig from '../assets/images/pig.svg'
+import add from '../assets/icons/add.svg'
+
 export const Home: React.FC = () => {
   return (
-    <div>Home </div>
+    <div>
+      <div flex justify-center item-center>
+        <img mt-20vh mb-20vh w='128px' h='130px' src={pig} alt="pig" />
+      </div>
+      <div px-16px>
+        <button text-white rounded-8px
+          h-48px w='100%' bg='#5c33be' b-none>开始记账</button>
+      </div>
+      <button fixed bottom-16px right-16px p-4px w-56px h-56px bg='#5c33be' rounded='50%' b-none text-white text-6xl >
+        <img src={add} alt='add' max-w="100%" max-h="100%"/>
+      </button>
+    </div>
   )
 }
