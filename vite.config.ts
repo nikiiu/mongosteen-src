e698d4a067c024a71ca5bc0f5293606aabc41e0e
nikiiu <2543954706@qq.com>
2024-07-11 21:58:36 +0800

    feat: 🎸 引入 svgsprites Vite 插件

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Unocss from 'unocss/vite'
 import { viteMockServe } from 'vite-plugin-mock'
+import { svgsprites } from './vite_plugins/svgsprites'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
@@ -12,5 +13,6 @@ export default defineConfig(({ command }) => ({
     Unocss(),
     react(),
     viteMockServe(),
+    svgsprites()
   ]
 }))
