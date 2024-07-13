6a5174e8ed8be233cc322305433e98035cebab22
nikiiu <2543954706@qq.com>
2024-07-13 15:12:29 +0800

    feat: 🎸 完成 ItemsSummary 组件

--- src/pages/ItemsPage/ItemsSummary.tsx
+++ src/pages/ItemsPage/ItemsSummary.tsx
@@ -1,5 +1,18 @@
 export const ItemsSummary: React.FC = () => {
   return (
-    <div>ItemsSummary </div>
+    <ol bg='#252A43' flex justify-between px-16px py-12px children-px-24px text-center items-center m-16px rounded-8px>
+      <li text="#e95b5e">
+        <div>收入</div>
+        <div>1000</div>
+      </li>
+      <li text="#53A867">
+        <div>支出</div>
+        <div>1000</div>
+      </li>
+      <li text="#FFFFFF">
+        <div>净收入</div>
+        <div>1000</div>
+      </li>
+    </ol>
   )
 }
