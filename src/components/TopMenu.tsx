5c24aad5b5cff201c52b2282d634acb30399c7ce
nikiiu <2543954706@qq.com>
2024-07-14 14:43:27 +0800

    feat: 🎸 给菜单添加滑动动画

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,19 +1,47 @@
+import { animated, useSpring } from '@react-spring/web'
+import { useState } from 'react'
 import { CurrentUser } from './TopMenu/CurrentUser'
 import { Menu } from './TopMenu/Menu'
 
 interface Props {
   onClickMask?: () => void
+  visible?: boolean
 }
 
-export const TopMenu: React.FC<Props> = ({ onClickMask }) => {
+export const TopMenu: React.FC<Props> = ({ onClickMask, visible }) => {
+  const [maskVisible, setMaskVisible] = useState(false)
+
+  const maskStyles = useSpring({
+    opacity: visible ? 1 : 0,
+    onStart: ({ value }) => {
+      if (value.opacity < 0.1) {
+        setMaskVisible(true)
+      }
+    },
+    onRest: ({ value }) => {
+      if (value.opacity < 0.1) {
+        setMaskVisible(false)
+      }
+    }
+  })
+
+  const menuStyles = useSpring({
+    opacity: visible ? 1 : 0,
+    transform: visible ? 'translateX(0%)' : 'translateX(-100%)',
+  })
+  const x = {
+    ...maskStyles,
+    visibility: (maskVisible ? 'visible' : 'hidden') as 'visible' | 'hidden'
+  }
+
   return (
     <>
-      <div fixed top-0 left-0 w="100%" h="100%" bg="[rgba(0,0,0,0.75)]" z="[calc(var(--z-menu)-1)]"
-        onClick={onClickMask}/>
-      <div fixed top-0 left-0 w-70vw max-w-20em h-screen flex flex-col z="[var(--z-menu)]">
+      <animated.div fixed top-0 left-0 w="100%" h="100%" bg="[rgba(0,0,0,0.75)]" z="[calc(var(--z-menu)-1)]"
+        onClick={onClickMask} style={x}/>
+      <animated.div style={menuStyles} fixed top-0 left-0 w-70vw max-w-20em h-screen flex flex-col z="[var(--z-menu)]">
         <CurrentUser className="grow-0 shrink-0" />
         <Menu className="grow-1 shrink-1" />
-      </div>
+      </animated.div>
     </>
 
   )
