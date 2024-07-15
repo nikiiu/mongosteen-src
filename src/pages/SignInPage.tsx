41950cfc5608f510db18fe06049618a2dfe14af9
nikiiu <2543954706@qq.com>
2024-07-15 19:54:29 +0800

    feat: 🎸 完成登录界面的样式

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -8,6 +8,26 @@ export const SignInPage: React.FC = () => {
       <Gradient>
         <TopNav title='登录' icon={<Icon name="back" className='w-24px h-24px' />} />
       </Gradient>
+       <div text-center pt-40px pb-16px>
+        <Icon name="logo" className='w-64px h-68px' />
+        <h1 text-32px text="#7878FF" font-bold>山竹记账</h1>
+      </div>
+      <form z-form>
+        <div>
+          <span z-form-label>邮箱地址</span>
+          <input z-input-text type="text" placeholder='请输入邮箱，然后点击发送验证码' />
+        </div>
+        <div>
+          <span z-form-label>验证码</span>
+          <div flex gap-x-16px>
+            <input z-input-text type="text" placeholder='六位数字'/>
+            <button z-btn>发送验证码</button>
+          </div>
+        </div>
+        <div mt-100px>
+          <button z-btn type="submit">登录</button>
+        </div>
+      </form>
     </div>
   )
 }
