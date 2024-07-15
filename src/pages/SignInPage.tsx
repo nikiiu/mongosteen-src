58876a5667ea2b7fa6e2e3a9382e87831da136f4
nikiiu <2543954706@qq.com>
2024-07-15 20:16:14 +0800

    feat: 🎸 表单提交事件可以获取到表单数据

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,8 +1,17 @@
+import type { FormEventHandler } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { TopNav } from '../components/TopNav'
+import { useSignInStore } from '../stores/useSignInStore'
 
 export const SignInPage: React.FC = () => {
+  const { data, setData } = useSignInStore()
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
+    console.log(data)
+  }
+
   return (
     <div>
       <Gradient>
@@ -12,15 +21,15 @@ export const SignInPage: React.FC = () => {
         <Icon name="logo" className='w-64px h-68px' />
         <h1 text-32px text="#7878FF" font-bold>山竹记账</h1>
       </div>
-      <form z-form>
+      <form z-form onSubmit={onSubmit}>
         <div>
           <span z-form-label>邮箱地址</span>
-          <input z-input-text type="text" placeholder='请输入邮箱，然后点击发送验证码' />
+          <input z-input-text value={data.email} onChange={e => setData({ email: e.target.value })} type="text" placeholder='请输入邮箱，然后点击发送验证码' />
         </div>
         <div>
           <span z-form-label>验证码</span>
           <div flex gap-x-16px>
-            <input z-input-text type="text" placeholder='六位数字'/>
+            <input z-input-text value={data.code} onChange={e => setData({ code: e.target.value })} type="text" placeholder='六位数字'/>
             <button z-btn>发送验证码</button>
           </div>
         </div>
