5d339f131ee88d5840fa7b9d7e3e49f81874155a
nikiiu <2543954706@qq.com>
2024-08-05 22:49:28 +0800

    fix: 🐛 解决倒计时 bug
    ✅ Closes: \

--- src/components/Input/SmsCodeInput.tsx
+++ src/components/Input/SmsCodeInput.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 type Props = {
   value?: string
@@ -7,35 +7,47 @@ type Props = {
   request?: () => Promise<unknown>
 }
 
+const maxCount = 60
 export const SmsCodeInput: React.FC<Props> = (props) => {
   const { value, placeholder, onChange, request } = props
-  const [started, setStarted] = useState(false)
-  const [count, setCount] = useState(5)
+  const [started, setStarted] = useState<Date>()
+  const [count, setCount] = useState(maxCount)
+  const timer = useRef<number>()
+  const clearTimer = () => {
+    if (timer.current) {
+      window.clearInterval(timer.current)
+      timer.current = undefined
+    }
+  }
   useEffect(() => {
-    if (!started) { return }
-    if (count === -1) {
-      setStarted(false)
-      setCount(5)
+    if (!started) {
+      clearTimer()
       return
-    }
-    const timer = setTimeout(() => {
-      setCount(count - 1)
-    }, 1000)
-    return () => {
-      window.clearTimeout(timer)
-    }
-  }, [started, count])
+     }
+      timer.current = window.setInterval(() => {
+        const seconds = Math.round((new Date().getTime() - started.getTime()) / 1000)
+         const count = maxCount - seconds
+        if (count < 0) {
+          setStarted(undefined)
+        }
+        setCount(count)
+      }, 1000)
+
+    return clearTimer
+  }, [started]
+  )
+
   const onClick = async () => {
     if (!request) { return }
     await request?.()
-    setStarted(true)
+    setStarted(new Date())
   }
   return (
     <div flex gap-x-16px>
       <input shrink-1 z-input-text type="text" placeholder={placeholder} w="[calc(45%-8px)]"
         value={value} onChange={e => onChange?.(e.target.value)} />
       {started
-        ? <button type="button" w="[calc(55%-8px)]" shrink-0 z-btn>{count}</button>
+        ? <button type="button" w="[calc(55%-8px)]" shrink-0 z-btn disabled text-gray>{count}秒后可重发</button>
         : <button type="button" w="[calc(55%-8px)]" shrink-0 z-btn onClick={onClick}>发送验证码</button>}
 
     </div>
