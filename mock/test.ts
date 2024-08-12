38ed0233bfbb1021e7f1d1005c29eb61c0217ad3
nikiiu <2543954706@qq.com>
2024-08-12 13:44:42 +0800

    feat: 🎸 获取 tags 的第一页数据

--- mock/test.ts
+++ mock/test.ts
@@ -2,9 +2,11 @@ import type { MockMethod } from 'vite-plugin-mock'
 import { meMock } from './me.mock'
 import { itemsMock } from './items.mock'
 import { sessionMock } from './session.mock'
+import { tagsMock } from './tags.mock'
 export default [
   meMock,
   itemsMock,
-  sessionMock
+  sessionMock,
+  tagsMock
 ] as MockMethod[]
 
